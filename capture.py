1c5011bfd4801a9cf5d9a44976db511b96c8b1d8
Daniel6 <dbishop.rwc@gmail.com>
2015-04-12 19:39:04 -0400

    hotkeys working

--- capture.py
+++ capture.py
@@ -1,10 +1,16 @@
 import gtk
 import time
+import sys
+sys.path.insert(0, './lib/pyxhook')
+import pyxhook
+import Image
 from Xlib import display
 
 class ZoneDesignator():
-	def __init__(self):
+	def __init__(self, bkgrnd):
 		self.dragging = False
+		self.running = True
+		self.bkgrnd = bkgrnd
 		self.top_left = []
 		self.bottom_right = []
 		self.root_window = gtk.gdk.get_default_root_window()
@@ -44,8 +50,11 @@ class ZoneDesignator():
 
 	def exposeEvent(self, widget, ev):
 		x , y, width, height = ev.area
+		# widget.window.draw_pixbuf(widget.get_style().fg_gc[gtk.STATE_NORMAL], self.bkgrnd, 0, 0, 0, 0)
 		widget.window.draw_drawable(widget.get_style().fg_gc[gtk.STATE_NORMAL],
 			pixmap, x, y, x, y, width, height)
+		if not self.running:
+			gtk.main_quit()
 
 	def configureEvent(self, widget, ev):
 		global pixmap
@@ -56,6 +65,7 @@ class ZoneDesignator():
 	def keyEvent(self, widget, ev, data=None):
 		if ev.keyval == 65307:
 			gtk.main_quit()
+			
 
 	def buttonDown(self, widget, ev):
 		if ev.button == 1:
@@ -73,29 +83,33 @@ class ZoneDesignator():
 			pixmap.draw_rectangle(widget.get_style().black_gc, True, rect[0], rect[1], rect[2], rect[3]) #Draw Black Rectangle defined by user
 			widget.queue_draw_area(0, 0, self.width, self.height)
 
+
 	def buttonUp(self, widget, ev):
 		if ev.button == 1:
 			self.bottom_right = self.mousepos()
 			self.dragging = False
-			self.window.set_opacity(0)
-			pixmap.draw_rectangle(widget.get_style().black_gc,
-				True, 0, 0, self.width, self.height)
+			self.running = False
 			widget.queue_draw_area(0, 0, self.width, self.height)
-			gtk.main_quit()
+			
 
 	def mousepos(self):
 		pos = display.Display().screen().root.query_pointer()._data
 		return pos["root_x"], pos["root_y"]
 
-if __name__ == "__main__":
-	myZoneDesignator = ZoneDesignator()
+def takeScreenshot(pixbuf):
+	myZoneDesignator = ZoneDesignator(pixbuf)
 	gtk.main()
-	size = [myZoneDesignator.bottom_right[0]-myZoneDesignator.top_left[0], myZoneDesignator.bottom_right[1]-myZoneDesignator.top_left[1]]
-	print "The size of the window is %d x %d" % (size[0], size[1])
+	try:
+		size = [myZoneDesignator.bottom_right[0]-myZoneDesignator.top_left[0], myZoneDesignator.bottom_right[1]-myZoneDesignator.top_left[1]]
+	except IndexError:
+		sys.exit()
+	screenie = gtk.gdk.Pixbuf(gtk.gdk.COLORSPACE_RGB, False, 8, size[0], size[1])
+	pixbuf.copy_area(myZoneDesignator.top_left[0], myZoneDesignator.top_left[1], size[0], size[1], screenie, 0, 0)
+	screenie.save("screenshot.png","png")
+
+
+if __name__ == "__main__":
+	size = [1600, 900]
 	pb = gtk.gdk.Pixbuf(gtk.gdk.COLORSPACE_RGB, False, 8, size[0], size[1])
-	pb = pb.get_from_drawable(myZoneDesignator.root_window,myZoneDesignator.root_window.get_colormap(), myZoneDesignator.top_left[0], myZoneDesignator.top_left[1],0,0,size[0],size[1])
-	if (pb != None):
-		pb.save("screenshot.png","png")
-		print "Screenshot saved to screenshot.png."
-	else:
-		print "Unable to get the screenshot."
+	pb = pb.get_from_drawable(gtk.gdk.get_default_root_window(), gtk.gdk.colormap_get_system(), 0,0,0,0,size[0],size[1])
+	takeScreenshot(pb)
---
+++ main.py
@@ -0,0 +1,43 @@
+import time
+import sys
+import subprocess
+sys.path.insert(0, './lib/pyxhook')
+import pyxhook
+from capture import takeScreenshot
+
+def keyDownEvent(event):
+	if event.ScanCode == 37: #If the scan code matches left control, signal that the ctrl button is pressed
+		global ctrl
+		ctrl = True
+
+	if event.ScanCode == 50: #If the scan code matches left shift, signal that the shift button is pressed
+		global shift
+		shift = True
+
+	if event.Ascii == 32: #If the ascii value matches spacebar, terminate the while loop
+		global running
+		running =  False
+	elif event.Ascii == 52: #If the ascii value matches '4', and both ctrl and shift are pressed, run screenshot.py
+		if ctrl and shift:
+			print("Click")
+			subprocess.call(["python2", "./capture.py"]) #Spawn a new process that takes a screenshot
+
+def keyUpEvent(event):
+	if event.ScanCode == 37: #If the scan code matches left control, signal that the ctrl button is not pressed
+		global ctrl
+		ctrl = False
+
+	if event.ScanCode == 50: #If the scan code matches left shift, signal that the shift button is not pressed
+		global shift
+		shift = False
+
+if __name__ == "__main__":
+	hookman = pyxhook.HookManager()
+	hookman.KeyDown = keyDownEvent #Bind keydown and keyup events
+	hookman.KeyUp = keyUpEvent
+	hookman.HookKeyboard()
+	hookman.start() #Start event listener
+	running = True
+	while running: #Stall
+		time.sleep(.1)
+	hookman.cancel() #Close listener
